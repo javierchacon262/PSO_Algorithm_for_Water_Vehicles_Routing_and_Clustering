c22c0d82f3dacd8a551d1d048704c374595248ca
Javier Chacon <javierchacon262@gmail.com>
2019-12-14 16:51:44 -0500

    Final commit

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.io as io
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -50,16 +49,16 @@ def seek_dist(cust, cust_list, vis):
             dist_vec.append([euc, i])
     #Ordenar nodos por cercania de menor a mayor distancia
     sort_dist = sorted(dist_vec, key=lambda x: x[0])
-    print('Metodo seek_dist: ')
-    print('')
+    #print('Metodo seek_dist: ')
+    #print('')
     for i in sort_dist:
         #selecciono el nodo mas cercano que no se encuentre dentro de la lista de visitados
         #print(i[1].num)
         if i[1].num not in vis:
             near = i[1]
             break
-    print('visited: ')
-    print(vis)
+    #print('visited: ')
+    #print(vis)
     return near
 ########################################################################################################
 ########################################################################################################
@@ -78,8 +77,9 @@ def sum_clus(clus_list):
 ########################################################################################################
 ########################################################################################################
 #Funcion de clustering para separar el problema en otros mas pequeños
-def clustering(cust_list, veh):
-    cap = np.copy(veh.cap)
+def clustering(cust_list, vehs):
+    veh = 0
+    cap = np.copy(vehs[veh].cap)
     perm = list(np.random.permutation(cust_list))
     init_node = perm[0]
     clus_list = []
@@ -97,19 +97,22 @@ def clustering(cust_list, veh):
                     visited.append(next_node.num)
                 elif i == len(perm) - 1:
                     clus_list.append(clus)
-                    print('cluster:')
-                    for j in clus:
-                        print(j.num)
-                    print('')
+                    #print('cluster:')
+                    #for j in clus:
+                    #    print(j.num)
+                    #print('')
                     clus = []
-                    cap = np.copy(veh.cap)
+                    veh += 1
+                    if veh == len(vehs):
+                        veh = 0
+                    cap = np.copy(vehs[veh].cap)
                 init_node = next_node
             else:
                 clus_list.append(clus)
-                print('cluster:')
-                for j in clus:
-                    print(j.num)
-                print('')
+                #print('cluster:')
+                #for j in clus:
+                #    print(j.num)
+                #print('')
     return clus_list
 ########################################################################################################
 ########################################################################################################
@@ -139,15 +142,20 @@ def seek_dep(cmn, deps):
 ########################################################################################################
 ########################################################################################################
 # Funcion de asignacion de clusters a depositos(hidrantes)
-def cluster_assign(cluster_list, deps):
+def cluster_assign(cluster_list, deps, vehs):
     deps2 = []
     for i in cluster_list:
         cm = mass_center(i)
         cmn = node(2, 0, 0, cm[0], cm[1])
         dep = seek_dep(cmn, deps)
         deps[dep][-1] = i
+    vcon = 0
     for i in deps:
         if i[4] != []:
+            i.append(vehs[vcon])
+            vcon += 1
+            if vcon == len(vehs):
+                vcon = 0
             deps2.append(i)
     return deps2
 ########################################################################################################
@@ -163,14 +171,16 @@ def init_population(cluster, pop_number):
 #Codificacion de la solucion al espacio PSO para cada cluster
 def code_solution(solution, xmin, xmax):
     coded = []
+    xmin_n = xmin.num
+    xmax_n = xmax.num
     n = len(solution)
     x = []
     y = []
     for i in solution:
         y.append(i.num)
-    print(y)
+    #print(y)
     for i in range(n):
-        xij = xmin + ((xmax - xmin)/(n*(y[i]-1+np.random.uniform())))
+        xij = xmin_n + ((xmax_n - xmin_n)/(n*(y[i]-1+np.random.uniform())))
         x.append(xij)
     return x
 ########################################################################################################
@@ -183,7 +193,7 @@ def decode_solution(solution, coded):
         idx = coded.index(min(coded))
         coded[idx]+=n
         sol[idx] = i
-    print(sol)
+    #print(sol)
     newsol = solution[sol]
     return newsol
 ########################################################################################################
@@ -194,14 +204,23 @@ def w_act(wmax, wmin, iter, k):
 ########################################################################################################
 ########################################################################################################
 #Actualizacion de velocidad para el PSO
-def v_act(xi, vi, w, cp, cq, pi, pg):
-    v = (w * vi) + (cp * np.random.rand() * (pi - xi)) + (cq * np.random.rand() * (pg - xi))
+def v_act(coded, vels, w, cp, cq, pi, pg):
+    v = []
+    for i in range(len(coded)):
+        xi = coded[i]
+        vi = vels[i]
+        v_i1 = (w * vi) + (cp * np.random.rand() * (pi - xi)) + (cq * np.random.rand() * (pg - xi))
+        v.append(v_i1)
     return v
 ########################################################################################################
 ########################################################################################################
 #Acrtualizacion de posicion para el PSO
-def x_act(xi, vi):
-    x = xi + vi
+def x_act(coded, vels):
+    x = []
+    for i in range(len(coded)):
+        xi = coded[i]
+        vi = vels[i]
+        x.append(xi + vi)
     return x
 ########################################################################################################
 ########################################################################################################
@@ -222,13 +241,22 @@ def obj_func_1(clus, vcap, pres_hid, pres_veh, m_vel):
 # Funcion Objetivo 2 para evaluar el comportamiento global de los cluster
 def obj_func_2(clusters, vcap, pres_hid, pres_veh, m_vel, tmax):
     t_total = 0
+    mayor = 0
+    obj_vec = []
     for i in clusters:
-        t_total += obj_func_1(i, vcap, pres_hid, pres_veh, m_vel)
+        obj = obj_func_1(i, vcap, pres_hid, pres_veh, m_vel)
+        obj_vec.append(obj)
+        if obj > mayor:
+            mayor = obj
+        t_total += obj
     if t_total > tmax:
         sigma = 1 + 0.1*(t_total - tmax)
+        bin = True
     else:
         sigma = 1
-    return t_total*sigma
+        bin = False
+    sol = [bin, t_total, t_total*sigma, mayor, obj_vec]
+    return sol
 ########################################################################################################
 ########################################################################################################
 # Algoritmo principal
@@ -236,7 +264,7 @@ def obj_func_2(clusters, vcap, pres_hid, pres_veh, m_vel, tmax):
 # Definicion de parametros iniciales & Lectura de datos de instancia desde excel
 df = pd.read_excel('./Instancias_Excel/InsTest1.xlsx', '1')
 ########################################################################################################
-sv = 15  # Numero de vehiculos
+sv = 5  # Numero de vehiculos
 vcap = df['v_cap'].values[0]
 ########################################################################################################
 df['coord_x'] = df['coord_x']/1000000
@@ -267,11 +295,12 @@ for i in range(sd):
     deps.append(node(1, i, dep_c[i], dep_x[i], dep_y[i]))
 ########################################################################################################
 # Creacion de vehiculos (cisternas)
+caps = [250, 150, 150, 100, 100]
 for i in range(sv):
-    vehs.append(veh(i, v_cap))
+    vehs.append(veh(i, caps[i]))
 ########################################################################################################
 # LLamada a la funcion clustering
-clusters = clustering(cust, vehs[0])
+clusters = clustering(cust, vehs)
 ########################################################################################################
 deps = list(zip(list(range(sd)), dep_x, dep_y, dep_c))
 deps = [list(i) for i in deps]
@@ -288,23 +317,102 @@ for i in clusters:
         cluster_list.append(i)
 ########################################################################################################
 # Asignacion de clusters a depositos (hidrantes)
-deps_ass = cluster_assign(cluster_list, deps)
+deps_ass = cluster_assign(cluster_list, deps, vehs)
 environ = []
+clus_dem = []
 for i in deps_ass:
     dep_i = node(1, i[0], i[3], i[1], i[2])
     environ.append([dep_i, i[4]])
+    # Recorrer clusters para sumar demandas
+    demsum_j = 0
+    for j in range(len(i[4])):
+        demsum_j += i[4][j].dem
+    clus_dem.append(demsum_j)
+#Suma de demandas de cada vehiculo
+vehs_dems = np.zeros((1, len(vehs)))
+vcon = 0
+for i in clus_dem:
+    vehs_dems[0][vcon] += i
+    vcon += 1
+    if vcon == len(vehs):
+        vcon = 0
+########################################################################################################
+# Asignar demandas de clusters a cisternas
+
 ########################################################################################################
 # Parametros
-cp = []
-cq = []
-wv = []
-iter = []
-m_vel = []
-pres_veh = []
-pres_hid = []
+cp = [0.4]
+cq = [0.4]
+wv = [1]
+iter = [100]
+init_p = [100]
+m_vel = [1]
+pres_veh = [1]
+pres_hid = [1]
+
+big_mat_sol = []
+big_mat_sol_vec = []
+for i in deps_ass:
+    mat_sol = []
+    mat_sol_vec = []
+    dep = node(1, i[0], i[3], i[1], i[2])
+    clu = i[4]
+    veh = i[5]
+    pop = init_population(clu, init_p[0])
+    #veh = 0
+    for j in pop:
+        sol = []
+        sol_vec = []
+        mi = min(j, key=lambda x: x.num)
+        ma = max(j, key=lambda x: x.num)
+        #Initial solution coding
+        coded = code_solution(j, mi, ma)
+        #Inicializacion aleatoria de las velocidades - distribucion uniforme.
+        vels = np.random.rand(len(coded))
+        #Inicializacion del mejor local y mejor global
+        pi = 9999999999.0
+        pg = 9999999999.0
+        for k in range(iter[0]):
+            w = w_act(2, 0.1, iter[0], k)
+            vels = v_act(coded, vels, w, cp[0], cq[0], pi, pg)
+            coded = x_act(coded, vels)
+            decoded = decode_solution(j, coded)
+            #caps[veh]
+            obj_k = obj_func_1([dep, decoded], veh.cap, pres_hid[0], pres_veh[0], m_vel[0])
+            if obj_k < pi:
+                sol.append(obj_k)
+                sol_vec.append(decoded)
+                pi = obj_k
+            coded = code_solution(decoded, mi, ma)
+
+        if sol[-1] < pg:
+            pg = sol[-1]
+            pg_vec = sol_vec[-1]
+
+        mat_sol.append(sol)
+        mat_sol_vec.append(sol_vec)
+    big_mat_sol.append(mat_sol)
+    big_mat_sol_vec.append(mat_sol_vec)
+########################################################################################################
+# Extraer los minimos de cada cluster, funcion objetivo, ruta y vehiculo
+mins = []
+mins_vec = []
+for i in range(len(big_mat_sol)): # Recorre cada cluster
+    min_per_clus = 999999999.0
+    for j in range(len(big_mat_sol[i])): # Recorre cada vector de soluciones para el cluster i
+        for k in range(len(big_mat_sol[i][j])): # Recorre las soluciones del vector i, j
+            if big_mat_sol[i][j][k] < min_per_clus:
+                min_per_clus = big_mat_sol[i][j][k]
+                min_per_clus_vec = big_mat_sol_vec[i][j][k]
+    mins.append(min_per_clus)
+    mins_vec.append(min_per_clus_vec)
+########################################################################################################
+# Calculo porcentaje de eficacia
 
 
+########################################################################################################
 #plots
+
 k = 0
 vec = []
 vecn = []
@@ -325,20 +433,65 @@ for i in range(len(clusters)):
     #    print(k)
 for i in range(sc):
     if i not in vec:
-        print(i)
+        #print(i)
         vecn.append(cust[i])
-        print(cust[i].num)
+        #print(cust[i].num)
         vecnx.append(cust[i].coord_x)
         vecny.append(cust[i].coord_y)
 vecnx = np.array(vecnx)
 vecny = np.array(vecny)
 vecx = np.array(vecx)
 vecy = np.array(vecy)
-
+########################################################################################################
+# Figura de los clusters
 fig = plt.figure(figsize=(10, 10))
 ax1 = fig.add_subplot(111)
-ax1.scatter(vecx, vecy, c='b', alpha=0.5, label='IN')
-ax1.scatter(vecnx, vecny, c='r', alpha=1, label='OUT')
-ax1.scatter(dep_x, dep_y, c='y', alpha=1, label='DEPS')
-plt.legend(loc='upper left');
+ax1.scatter(vecx, vecy, c='b', alpha=0.5, label='Clientes')
+ax1.scatter(dep_x, dep_y, c='r', alpha=1, label='Depositos')
+plt.legend(loc='upper left')
 plt.show()
+########################################################################################################
+# Plot No.1
+x_veh = []
+for i in range(len(vehs)):
+    x_veh.append(i)
+x_veh = np.array(x_veh)
+fig2 = plt.figure(figsize=(10, 10))
+ax2 = fig2.add_subplot(111)
+ax2.bar(x_veh, vehs_dems[0])
+ax2.title = plt.title('Demanda Por Vehículo')
+ax2.xlabel = plt.xlabel('Vehículos')
+ax2.ylabel = plt.ylabel('Demanda Atendida')
+plt.show()
+########################################################################################################
+# Plot No.2
+x_clus = []
+for i in range(len(deps_ass)):
+    x_clus.append(i)
+x_clus = np.array(x_clus)
+fig3 = plt.figure(figsize=(10, 10))
+ax3 = fig3.add_subplot(111)
+ax3.bar(x_clus, clus_dem)
+ax3.title = plt.title('Demanda Por Cluster')
+ax3.xlabel = plt.xlabel('Cluster')
+ax3.ylabel = plt.ylabel('Demanda Atendida')
+plt.show()
+########################################################################################################
+# Plot No.3
+fig4 = plt.figure(figsize=(10, 10))
+ax4 = fig4.add_subplot(111)
+ax4.bar(x_clus, mins)
+ax4.title = plt.title('Funcion Objetivo por Cluster')
+ax4.xlabel = plt.xlabel('Cluster')
+ax4.ylabel = plt.ylabel('Funcion Objetivo')
+plt.show()
+########################################################################################################
+# Plot No.4
+fig5 = plt.figure(figsize=(10, 10))
+ax5 = fig5.add_subplot(111)
+ax5.scatter(clus_dem, mins)
+ax5.title = plt.title('Funcion Objetivo por Demanda de Cluster')
+ax5.xlabel = plt.xlabel('Demanda Atendida')
+ax5.ylabel = plt.ylabel('Funcion Objetivo')
+plt.show()
+########################################################################################################
